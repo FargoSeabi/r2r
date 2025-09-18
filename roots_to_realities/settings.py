3ef5b9c7e30a616f410b7ded5e5b22ccd73e0125
FargoSeabi <seabimoses7@gmail.com>
2025-09-18 13:59:59 +0200

    Fix ALLOWED_HOSTS for Render deployment

--- roots_to_realities/settings.py
+++ roots_to_realities/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-ds4ltg3nuel1o%lho)se#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,roots-to-realities.onrender.com').split(',')
 
 
 # Application definition
